366928655a950ad05f2b966075f814fa621f7694
ShanigarapuVinay <vinay.shanigarapu@zemosolabs.com>
2024-05-28 21:53:18 +0530

    Revert "Change in server.js"
    This reverts commit 8a0d7296e5cb9fa4312af08f6984fbc88b17a0dc.

--- src/Api/server.js
+++ src/Api/server.js
@@ -185,9 +185,9 @@ app.delete('/api/cart', async (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
